706744154f4ba1056fd0b89ed1d5822a29d8e9dd
flyingbutton <thedrafttin@gmail.com>
2023-11-19 16:20:35 +0800

    update

--- lc-40.go
+++ lc-40.go
@@ -1,19 +1,30 @@
 func combinationSum2(candidates []int, target int) [][]int {
-	curCombination := make([]int, len(candidates))
-	visited := make([]int, len(candidates))
-	ans := [][]int{}
-	helper(candidates, 0, &curCombination, 0, &ans, target, visited)
-	return ans
+	curCombination := make([]string, len(candidates))
+	visited := make([]bool, len(candidates))
+	sort.Ints(candidates)
+	ans := map[string]bool{}
+	helper(candidates, 0, 0, &curCombination, 0, ans, target, visited)
+	result := make([][]int, 0, len(ans))
+	for key, _ := range ans {
+		item := strings.Split(key, ",")
+		numArr := make([]int, len(item))
+		for i := 0; i < len(item); i++ {
+			numArr[i], _ = strconv.Atoi(item[i])
+		}
+		result = append(result, numArr)
+	}
+	return result
 }
 
-func helper(candidates []int, curSum int, curCombination *[]int, curPos int, ans *[][]int, target int, visited []bool) {
+func helper(candidates []int, pos int, curSum int, curCombination *[]string, curPos int, ans map[string]bool, target int, visited []bool) {
 	if curSum == target {
-		item := make([]int, curPos)
-		copy(item, curCombination)
-		*ans = append(*ans, item)
+		item := make([]string, curPos)
+		copy(item, *curCombination)
+		key := strings.Join(item, ",")
+		ans[key] = true
 		return
 	}
-	for i, _ := range candidates {
+	for i := pos; i < len(candidates); i++ {
 		if visited[i] {
 			continue
 		}
@@ -21,8 +32,8 @@ func helper(candidates []int, curSum int, curCombination *[]int, curPos int, ans
 			continue
 		}
 		visited[i] = true
-		(*curCombination)[curPos] = candidates[i]
-		helper(candidates, curSum+candidates[i], curCombination, curPos+1, ans, target, visited)
+		(*curCombination)[curPos] = strconv.Itoa(candidates[i])
+		helper(candidates, i+1, curSum+candidates[i], curCombination, curPos+1, ans, target, visited)
 		visited[i] = false
 	}
 }
