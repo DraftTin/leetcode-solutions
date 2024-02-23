4d76c6067757177dc402d59f9f9788d7d91a2073
flyingbutton <thedrafttin@gmail.com>
2024-02-23 20:21:21 +0800

    update

--- lc-787.go
+++ lc-787.go
@@ -1,5 +1,5 @@
-// if we use BFS and Dijkstra to solve a shortest-path problem, we shouldn't reflect the update of the next
-// level into this level
+package main
+
 type Node struct {
 	dist        int
 	adjacencies []Edge
@@ -17,22 +17,33 @@ func findCheapestPrice(n int, flights [][]int, src int, dst int, k int) int {
 		nodes[i].dist = math.MaxInt32
 	}
 	for _, flight := range flights {
-		nodes[flight[0]-1].adjacencies = append(nodes[i].adjacencies, Edge{to: flight[1] - 1, weight: flight[2]})
+		nodes[flight[0]].adjacencies = append(nodes[flight[0]].adjacencies, Edge{to: flight[1], weight: flight[2]})
 	}
-	queue := []int{k - 1}
+	queue := [][2]int{[2]int{src, 0}}
 	level := 0
 	size := 1
 	for level <= k && len(queue) != 0 {
-		index := queue[0]
+		update := queue[0]
 		queue = queue[1:]
-		if index == k-1 && ans > nodes[index].dist {
-			ans = nodes[index].dist
+		if nodes[update[0]].dist > update[1] {
+			nodes[update[0]].dist = update[1]
+		} else {
+			continue
 		}
+		index := update[0]
 		for _, edge := range nodes[index].adjacencies {
 			if edge.weight+nodes[index].dist < nodes[edge.to].dist {
-				nodes[edge.to].dist = edge.weight + nodes[index].dist
-				queue = append(queue, edge.to)
+				newDist := edge.weight + nodes[index].dist
+				queue = append(queue, [2]int{edge.to, newDist})
+				if edge.to == dst && ans > newDist {
+					ans = newDist
+				}
 			}
+		}
+		size--
+		if size == 0 {
+			size = len(queue)
+			level++
 		}
 	}
 	if ans == math.MaxInt32 {
